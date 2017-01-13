802369488c5a7ff5cafbd0e07cafd7b8f282ee71
Clark Malmgren <cmalmgren@grubhub.com>
2017-01-13 08:22:02 -0600

    Make the header functional with links

--- src/app/components/molecules/header.ts
+++ src/app/components/molecules/header.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
 
-interface Link {
-  name: string,
-  url: string
+class Link {
+  constructor(
+    public name: string,
+    public icon: string,
+    public url: string
+  ) { }
 }
 
 @Component({
   selector: 'sda-header',
   templateUrl: './header.html',
-  styleUrls: [ './header.scss' ]
+  styleUrls: ['./header.scss']
 })
 export class Header {
 
+  links = [
+    new Link('Home', 'home', '/'),
+    new Link('About Us', 'accessibility', '/about'),
+    new Link('Giving', 'trending_up', '/giving'),
+    new Link('Messages', 'school', '/messages'),
+    new Link('Get Connected', 'power', '/get-connected'),
+    new Link('Serve', 'people', '/serve'),
+    new Link('Locations', 'public', '/locations'),
+    new Link('Missions', 'textsms', '/missions'),
+    new Link('Newsletter Sign-up', 'markunread_mailbox', 'newsletter'),
+    new Link('Events', 'event', '/events'),
+  ];
+
+  opened = false;
 }
