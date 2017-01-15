0d562c086fc39d483607b74323b0df9951900ba0
Clark Malmgren <cmalmgren@grubhub.com>
2017-01-14 23:14:34 -0600

    Add support for both beta and prod deployments

---
+++ src/environments/environment.beta.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyBq1DN3NMR_8SMdfEYqoMn0xsUbPZoGOuI",
+    authDomain: "bolingbrook-church-beta.firebaseapp.com",
+    databaseURL: "https://bolingbrook-church-beta.firebaseio.com",
+    storageBucket: "bolingbrook-church-beta.appspot.com",
+    messagingSenderId: "525536092170"
+  }
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
 
   firebaseConfig : {
-    apiKey: "AIzaSyBq1DN3NMR_8SMdfEYqoMn0xsUbPZoGOuI",
-    authDomain: "bolingbrook-church-beta.firebaseapp.com",
-    databaseURL: "https://bolingbrook-church-beta.firebaseio.com",
-    storageBucket: "bolingbrook-church-beta.appspot.com",
-    messagingSenderId: "525536092170"
+    apiKey: "AIzaSyC_pmwFlv6YDOJHwdVFEdsl24Rv89xwmQI",
+    authDomain: "bolingbrook-church.firebaseapp.com",
+    databaseURL: "https://bolingbrook-church.firebaseio.com",
+    storageBucket: "bolingbrook-church.appspot.com",
+    messagingSenderId: "110300192266"
   }
 };
