230d51d765a1101dfa07b2882f15bf578bbb3947
Clark Malmgren <cmalmgren@gmail.com>
2017-07-07 22:00:06 -0500

    Restore Ustream (#42)
    * Reinstate UStream as an Option
    * Create a service to return feature toggles
    * Bump the version (1.1.4)

--- src/app/components/organisms/index.ts
+++ src/app/components/organisms/index.ts
@@ -1,7 +1,8 @@
 import { Header }               from './header';
 import { Footer }               from './footer';
 import { SermonListComponent }  from './sermon-list';
+import { UStream }              from './ustream';
 
 export const ORGANISMS = [
-  Header, Footer, SermonListComponent
+  Header, Footer, SermonListComponent, UStream
 ];
