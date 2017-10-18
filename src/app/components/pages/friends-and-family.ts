e9a491f57d7a143fa29db33939ed9149e4e1803f
Clark Malmgren <cmalmgren@gmail.com>
2017-10-17 22:23:57 -0500

    Friends and Family Sabbath Updates (closes #86)

--- src/app/components/pages/friends-and-family.ts
+++ src/app/components/pages/friends-and-family.ts
@@ -1,7 +1,8 @@
 import { Component }    from '@angular/core';
 
 @Component({
-  templateUrl: './friends-and-family.html'
+  templateUrl: './friends-and-family.html',
+  styleUrls: [ './friends-and-family.scss' ]
 })
 export class FriendsAndFamilyComponent {
 }
