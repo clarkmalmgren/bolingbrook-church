62672f0044fda7f5ba9a711f13f8212376593dd9
Clark Malmgren <cmalmgren@gmail.com>
2017-12-05 23:59:34 -0600

    Build Docker Image #91

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+docker --version
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+
+eval $(aws ecr get-login --region us-east-1)
+docker push 971947037958.dkr.ecr.us-east-1.amazonaws.com/bolingbrook-church/website
