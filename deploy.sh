54f7beb3874dfd22214dacdc66dc8cc1565e0863
Clark Malmgren <cmalmgren@gmail.com>
2017-12-07 17:33:30 -0600

    Automatically tag latest for docker builds

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
+REPO="971947037958.dkr.ecr.us-east-1.amazonaws.com/bolingbrook-church/website"
+
 echo "Starting Deploy Script"
 
 docker --version
+docker tag ${REPO}:${TRAVIS_TAG} ${REPO}:latest
+
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 
 eval $(aws ecr get-login --region us-east-1)
-docker push 971947037958.dkr.ecr.us-east-1.amazonaws.com/bolingbrook-church/website
+docker push ${REPO}
