d677c2c31a8cbc05b035f8ecb0f6b26d4c49b7f4
Clark Malmgren <cmalmgren@gmail.com>
2018-03-18 15:11:04 -0500

    Add support for multiple service videos (resolves #105)

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,7 +1,6 @@
 import { NgModule }                                   from '@angular/core';
 import { ServerModule }                               from '@angular/platform-server';
 import { ModuleMapLoaderModule }                      from '@nguniversal/module-map-ngfactory-loader';
-import { RESPONSE }                                    from '@nguniversal/express-engine/tokens'
 import * as express                                   from 'express';
 import { AppModule }                                  from './app.module';
 import { AppComponent }                               from './app.component';
